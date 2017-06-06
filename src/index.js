d7d82c1647159f4e3db0c94fc71af82a9cfb7a21
david.merrick <david.merrick@jivesoftware.com>
2017-06-06 15:36:28 -0700

    Added truncation of classified Tweets array

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ var index = async (event, context, callback) => {
     const S3_REGION = process.env.S3_REGION || "us-west-2";
     const S3_BUCKET = process.env.S3_BUCKET;
     const FILENAME = process.env.FILENAME;
+    const MAX_TWEETS = process.env.MAX_TWEETS || 100; // Max number of Tweets to store data on
 
     if(!S3_BUCKET || !FILENAME){
         return callback(new Error("Please specify FILENAME and S3_BUCKET variables."));
@@ -31,6 +32,7 @@ var index = async (event, context, callback) => {
     let tweetsToClassify = TweetsUtils.filterAlreadyClassifiedTweets(classifiedTweets, allTweets);
     let newlyClassifiedTweets = await TweetsUtils.classifyTweets(tweetsToClassify);
     let newData = newlyClassifiedTweets.concat(classifiedTweets);
+    newData = newData.slice(0, MAX_TWEETS);
     params = {
         Body: JSON.stringify(newData),
         ACL: "public-read",
